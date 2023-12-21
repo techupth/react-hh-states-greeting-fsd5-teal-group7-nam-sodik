4c780094a77c92e4b202a42aba7ac1dde27c0e80
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2023-12-21 13:18:59 +0700

    add greeting-states

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("Greeting Message");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container" onChange={message}>
+        {message}
+      </div>
+
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button
+          onClick={() => {
+            setMessage("สวัสดี!");
+          }}
+        >
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            setMessage("Hi!");
+          }}
+        >
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            setMessage("你好!");
+          }}
+        >
+          你好!
+        </button>
+
+        <button
+          onClick={() => {
+            setMessage("Greeting Message");
+          }}
+        >
+          reset
+        </button>
       </div>
     </div>
   );
